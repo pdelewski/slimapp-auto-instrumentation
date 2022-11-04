e83b89118d1bd745660f710b0f68559bab9f828b
Przemek Delewski <pdelewski@sumologic.com>
2022-11-04 18:26:38 +0100

    removing unneeded usings

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use OpenTelemetry\API\Trace\AbstractSpan;
-use OpenTelemetry\API\Trace\SpanKind;
-use OpenTelemetry\SDK\Trace\Tracer;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
